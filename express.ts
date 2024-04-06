8de73d43b09412794121da2eb0e60c902b0ac0ac
nirajmuttur <nirajmuttur@gmail.com>
2024-04-06 19:31:52 +0530

    added swagger js doc

--- express.ts
+++ express.ts
@@ -6,23 +6,53 @@ import cookieParser from 'cookie-parser'
 import authRoutes from './routes/authRoutes.ts'
 import playListRoutes from './routes/playListRoutes.ts';
 import trackRoutes from './routes/trackRoutes.ts';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
 
 dotenv.config()
 const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true 
+    credentials: true
 }))
 app.use(helmet())
-app.use(express.json({limit:"20kb"}))
-app.use(express.urlencoded({extended: true,limit:"20kb"}))
+app.use(express.json({ limit: "20kb" }))
+app.use(express.urlencoded({ extended: true, limit: "20kb" }))
 app.use(cookieParser())
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Your API Documentation',
+            version: '1.0.0',
+            description: 'Documentation for your API',
+        },
+        components: {
+            securitySchemes:{
+                bearerAuth:{           
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT' 
+                }
+            }
+        },
+        security: [{
+            bearerAuth: []
+        }],
+    },
+    // Paths to files containing OpenAPI definitions
+    apis: ['./routes/*.ts'], // Assuming your route files are in a 'routes' directory
+};
+
+const specs = swaggerJsdoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.use('/api', authRoutes)
 app.use('/api', playListRoutes)
-app.use('/api',trackRoutes)
+app.use('/api', trackRoutes)
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
--- routes/playListRoutes.ts
+++ routes/playListRoutes.ts
@@ -1,10 +1,80 @@
 import express from 'express';
-import { getPlayList,getPlayListTracks } from '../controller/playListController.ts';
+import { getPlayList, getPlayListTracks } from '../controller/playListController.ts';
 import { spotifyAuthMiddelware } from '../controller/authController.ts';
 
 const router = express.Router();
 
-router.route('/getPlayList').get(spotifyAuthMiddelware,getPlayList)
-router.route('/playlists/:playlistId/tracks').get(spotifyAuthMiddelware,getPlayListTracks)
+/**
+ * @swagger
+ * components:
+ *    schemas:
+ *      Playlist: 
+ *          type: object
+ *          properties: 
+ *              id:  
+ *                  type: number 
+ *              name:  
+ *                  type: string
+ *              images: 
+ *                  type: array
+ * 
+ *      PlaylistTrack:
+ *          type: object
+ *          properties:
+ *              name: 
+ *                  type: string
+ *              artist:
+ *                  type: array
+ *              image: 
+ *                  type: object
+ *                  additionalProperties:
+ *                     $ref: '#components/schemas/Image'
+ *      
+ * 
+ *      Image: 
+ *          type: object
+ *          properties:
+ *              height: 
+ *                  type: number
+ *              url: 
+ *                  type: string
+ *              width: 
+ *                  type: number
+ *                                               
+ */
+/**
+ * @swagger
+ * /api/getPlayList:
+ *   get:
+ *     description: Get playlists
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *                  $ref: '#components/schemas/Playlist'
+ */
+router.route('/getPlayList').get(spotifyAuthMiddelware, getPlayList)
+
+/**
+ * @swagger
+ * /api/playlists/{playlistId}/tracks:
+ *   get:
+ *     description: Get playlists Track
+ *     parameters:
+ *          - in: path
+ *            name: playlistId
+ *            required: true
+ *            description: Playlist id
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *                  $ref: '#components/schemas/PlaylistTrack'
+ */
+router.route('/playlists/:playlistId/tracks').get(spotifyAuthMiddelware, getPlayListTracks)
 
 export default router
